7837ac8e0e3443597ecde04e2ddb675c539ae0f9
hathiep <havanthiep2308@gmail.com>
2023-05-02 22:21:58 +0700

    Hoàn thành thuật toán BFS và ỉn ra đáp án

--- src/java/controll/AlgorithmControl.java
+++ src/java/controll/AlgorithmControl.java
@@ -15,6 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import model.BFSAlgorithm;
 
 /**
  *
@@ -30,105 +31,7 @@ public class AlgorithmControl extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    private static ArrayList<LinkedList<Integer>> canh = new ArrayList<LinkedList<Integer>>();
-    private static boolean[] chuaxet = new boolean[10000];
-    private static ArrayList<Integer> trace = new ArrayList<Integer>();
-    private static String str = "";
-    
-    private static void BFS(int u){
-            LinkedList<Integer> q = new LinkedList<Integer>();
-            q.offer(u);
-            chuaxet[u] = true;
-            while (!q.isEmpty()){
-                    u = q.peek();
-                    q.poll();
-                    for (int i : canh.get(u))
-                    {
-                            if (!chuaxet[i] && i<trace.size())
-                            {
-                                    q.offer(i);
-                                    trace.set(i, u);
-                                    chuaxet[i] = true;
-                            }
-                    }
-            }
-    }
-    private static String tim(int s, int e){
-            if (trace.get(e) == 0){
-                    return "Khong tim duoc duong di1";
-            }
-            ArrayList<Integer> res = new ArrayList<Integer>();
-            while (e != s){
-                    if (e == 0)
-                    {
-                            return "Khong tim duoc duong di2";
-                    }
-                    res.add(e);
-                    e = trace.get(e);
-            }
-            res.add(e);
-            Collections.reverse(res);
-            String st = "" + str.charAt(res.get(0));
-            for (int i = 1; i < res.size(); i++)
-            {
-                    st+="->"+str.charAt(res.get(i));
-            }
-            return st;
-    }
-    
-    public static String SolveBFS(int n, int m, String start_node, String finish_node, String list_edge){
-        int s=0, e=0, i;
-        char X, Y, S, E;
-        S = start_node.charAt(0);
-        E = finish_node.charAt(0);
-        canh.clear();
-        trace.clear();
-        for(i=0; i<=n; i++){
-            LinkedList<Integer> q = new LinkedList<>();
-            canh.add(q);
-            q.offer(0);
-            trace.add(0);
-            chuaxet[i] = false;
-        }
-        int x = 0, y = 0, t = 0;
-        while(m-->0){
-            X = list_edge.charAt(t++);
-            Y = list_edge.charAt(t++);
-            int k=0;
-            for(i=0; i<str.length(); i++){
-                if(str.charAt(i)==X){
-                    x = i;
-                    k = 1;
-                    break;
-                }
-            }
-            if(k==0){
-                str+= X;
-                x=str.length()-1;
-            }
-            else k=0;
-            for(i=0; i<str.length(); i++){
-                if(str.charAt(i)==Y){
-                    y = i;
-                    k = 1;
-                    break;
-                }
-                if(k==0){
-                    str+= Y;
-                    y=str.length()-1;
-                }
-                LinkedList<Integer> z = canh.get(x);
-                z.offer(y);
-                canh.set(x, z);
-            }
-        }
-        for(i=0; i<str.length(); i++){
-            if(str.charAt(i)==S) s=i;
-            if(str.charAt(i)==E) e=i;
-        }
-        BFS(s);
-        return tim(s, e);
-    }
+  
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
@@ -138,8 +41,8 @@ public class AlgorithmControl extends HttpServlet {
         String start_node = request.getParameter("start_node");
         String finish_node = request.getParameter("finish_node");
         String list_edge = request.getParameter("list_input_edge");
-
-        String ans = SolveBFS(n, m, start_node, finish_node, list_edge);
+        BFSAlgorithm bfs = new BFSAlgorithm(n, m, start_node, finish_node, list_edge);
+        String ans = bfs.Solve();
         if(ans != null){
             request.setAttribute("ans", ans);
             request.getRequestDispatcher("algorithm.jsp").forward(request, response);
---
+++ src/java/model/BFSAlgorithm.java
@@ -0,0 +1,124 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author havanthiep
+ */
+public class BFSAlgorithm {
+    public static ArrayList<LinkedList<Integer>> canh = new ArrayList<LinkedList<Integer>>();
+    public static boolean[] chuaxet = new boolean[10000];
+    public static ArrayList<Integer> trace = new ArrayList<Integer>();
+    public static String str = "";
+    public int n, m;
+    public String start_node, finish_node, list_edge;
+    
+    private static void BFS(int u){
+            LinkedList<Integer> q = new LinkedList<Integer>();
+            q.offer(u);
+            chuaxet[u] = true;
+            while (!q.isEmpty()){
+                u = q.peek();
+                q.poll();
+                for (int i : canh.get(u)){
+                    if (!chuaxet[i]){
+                        q.offer(i);
+                        trace.set(i, u);
+                        chuaxet[i] = true;
+                    }
+                }
+            }
+    }
+    private static String tim(int s, int e){
+            if (trace.get(e) == 0){
+                return "Khong tim duoc duong di1";
+            }
+            ArrayList<Integer> res = new ArrayList<Integer>();
+            while (e != s){
+                if (e == 0){
+                    return "Khong tim duoc duong di2";
+                }
+                res.add(e);
+                e = trace.get(e);
+            }
+            res.add(e);
+            Collections.reverse(res);
+            String st = "" + str.charAt(res.get(0));
+            for (int i = 1; i < res.size(); i++){
+                st+="->"+str.charAt(res.get(i));
+            }
+            return st;
+    }
+
+    public BFSAlgorithm(int n, int m, String start_node, String finish_node, String list_edge) {
+        this.n = n;
+        this.m = m;
+        this.start_node = start_node;
+        this.finish_node = finish_node;
+        this.list_edge = list_edge;
+    }
+    
+    
+    public String Solve(){
+        int s=0, e=0;
+        char X, Y, S, E;
+        S = start_node.charAt(0);
+        E = finish_node.charAt(0);
+        canh.clear();
+        trace.clear();
+        for(int i=1; i<=n; i++){
+            LinkedList<Integer> q = new LinkedList<>();
+            canh.add(q);
+            q.offer(0);
+            trace.add(0);
+            chuaxet[i] = false;
+        }
+        int x = 0, y = 0;
+        str = "" + list_edge.charAt(0);
+        for(int i=1; i<list_edge.length(); i++){
+            int k = 0;
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j) == list_edge.charAt(i)){
+                    k = 1;
+                    break;
+                }
+            }
+            if(k == 0){
+                str+= list_edge.charAt(i);
+            }
+        }
+        for(int i=0; i<2*m; i+=2){
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j)==list_edge.charAt(i)){
+                    x = j;
+                    break;
+                }
+            }
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(j) == list_edge.charAt(i+1)){
+                    y = j;
+                    break;
+                }
+            }
+            LinkedList<Integer> z = canh.get(x);
+            z.offer(y);
+            canh.set(x, z);
+        }
+        System.out.println(n + "*" + m + "*" + str + "*" + S + "*" + E);
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)==S) s=i;
+            if(str.charAt(i)==E) e=i;
+        }
+        System.out.println(s + "*" + e);
+        BFS(s);
+        return tim(s, e);
+    }
+
+}
